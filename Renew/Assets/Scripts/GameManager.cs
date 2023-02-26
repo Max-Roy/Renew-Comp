6d2ecc8a4748ac059f0b8ee7c776c66339384268
Max-Roy <volleywrath@gmail.com>
2023-02-25 17:14:06 -0800

    Almost finished vid

--- Renew/Assets/Scripts/GameManager.cs
+++ Renew/Assets/Scripts/GameManager.cs
@@ -8,26 +8,53 @@ public class GameManager : MonoBehaviour
     public int gold;
     public Text goldDisplay;
 
-    //private Building buildingToPlace;
+    private Building buildingToPlace;
     public GameObject grid;
 
-    private void Start()
-    {
-        //cameraFollow.Setup(() => cameraFollowPosition, () => 80f);
-    }
+    public CustomCursor customCursor;
+
+    public Tile[] tiles;
 
     private void Update()
     {
         goldDisplay.text = gold.ToString();
+
+        if (Input.GetMouseButtonDown(0) && buildingToPlace != null)
+        {
+            Tile nearestTile = null;
+            float nearestDistance = float.MaxValue;
+            foreach(Tile tile in tiles)
+            {
+                float dist = Vector2.Distance(tile.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                if(dist < nearestDistance)
+                {
+                    nearestDistance = dist;
+                    nearestTile = tile;
+                }
+            }
+            if(nearestTile.isOccupied == false)
+            {
+                Instantiate(buildingToPlace, nearestTile.transform.position, Quaternion.identity);
+                buildingToPlace = null;
+                nearestTile.isOccupied = true;
+                grid.SetActive(false);
+                customCursor.gameObject.SetActive(false);
+                Cursor.visible = true;
+            }
+        }
     }
 
-    //public void BuyBuilding(Building building)
-    //{
-    //    if (gold >= building.cost)
-    //    {
-    //        gold -= building.cost;
-    //        buildingToPlace = building;
-    //        grid.SetActive(true);
-    //    }
-    //}
+    public void BuyBuilding(Building building)
+    {
+        if (gold >= building.cost)
+        {
+            customCursor.gameObject.SetActive(true);
+            customCursor.GetComponent<SpriteRenderer>().sprite = building.GetComponent<SpriteRenderer>().sprite;
+            Cursor.visible = false;
+
+            gold -= building.cost;
+            buildingToPlace = building;
+            grid.SetActive(true);
+        }
+    }
 }
