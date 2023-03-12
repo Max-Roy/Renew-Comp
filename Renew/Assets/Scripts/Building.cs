baab3938f4335a406858b2e0e3e2d10792eeae40
Max-Roy <volleywrath@gmail.com>
2023-03-12 10:40:26 -0700

    Shadows on buildings

--- Renew/Assets/Scripts/Building.cs
+++ Renew/Assets/Scripts/Building.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public int cost;
+    public int goldIncrease;
+
+
+    public float timeBetweenIncrease;
+    private float nextIncrese;
+
+
+    private GameManager GM;
+
+    private void Start()
+    {
+        GM = FindObjectOfType<GameManager>();
+ 
+    }
+
+    private void Update()
+    {
+        if (Time.time > nextIncrese)
+        {
+            nextIncrese = Time.time + timeBetweenIncrease;
+            GM.gold += goldIncrease;
+        }
+    }
 }
+
